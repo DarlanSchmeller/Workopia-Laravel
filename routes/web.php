f01aefb6a370fc3840b17e561d948f9d21d7d1b5
Darlan Rodrigues Schmeller <darlanschimeller@gmail.com>
2025-07-09 11:02:06 -0300

    Add posts route and implement numbers only for id param

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/jobs', function () {
+    return view('welcome') . '<h1>Available Jobs</h1>';
+})->name('jobs');
+
+Route::get('/posts/{id}', function (string $id) {
+    return 'Post ' . $id;
+})->whereNumber('id');
+
+Route::get('/posts/{id}/comments/{commentId}', function (string $id, string $commentId) {
+    return 'Post ' . $id . $commentId;
+});
