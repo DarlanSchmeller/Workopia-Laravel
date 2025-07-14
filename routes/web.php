0880e5ade87dba0b294deb702984c13390bf76a5
Darlan Rodrigues Schmeller <darlanschimeller@gmail.com>
2025-07-14 11:18:39 -0300

    Add controllers for every route, remove closures and unused routes

--- routes/web.php
+++ routes/web.php
@@ -2,52 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
+use App\Http\Controllers\JobController;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/jobs', function () {
-    $title = 'Available Jobs';
-    $jobs = [
-       'Web Developer',
-       'Database Admin',
-       'Software Engineer',
-       'Systems Analyst'
-    ];
-
-    return view('jobs.index', compact('title', 'jobs'));
-})->name('jobs');
-
-Route::get('/jobs/create', function () {
-    return view('jobs.create');
-})->name('jobs.create');
-
-Route::get('/posts/{id}', function (string $id) {
-    return 'Post ' . $id;
-})->whereNumber('id');
-
-Route::get('/posts/{id}/comments/{commentId}', function (string $id, string $commentId) {
-    return 'Post ' . $id . $commentId;
-});
-
-Route::get('/test', function(Request $request) {
-    return [
-        'method' => $request->method(),
-        'url' => $request->url(),
-        'path' => $request->path(),
-        'fullUrl' => $request->fullUrl(),
-        'ip' => $request->ip(),
-        'userAgent' => $request->userAgent(),
-        'header' => $request->header()
-    ];
-});
-
-Route::get('/download', function() {
-    return response()->json(['name' => 'john doe'])->cookie('name', 'John doe');
-});
-
-Route::get('/read-cookie', function(Request $request) {
-    $cookieValue = $request->cookie('name');
-    return response()->json(['cookie' => $cookieValue]);
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/jobs', [JobController::class, 'index'])->name('jobs');
+Route::get('/jobs/create', [JobController::class, 'create'])->name('jobs.create');
