7ddc3f1ce1b4cea1d9e887951f3bf9ad4ee2cbc3
Darlan Rodrigues Schmeller <darlanschimeller@gmail.com>
2025-07-14 11:31:03 -0300

    Create store method for JobController, use post request params

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -7,5 +7,12 @@
 </head>
 <body>
     <h1>Create new job</h1>
+
+    <form action="/jobs" method="POST">
+        @csrf
+        <input name="title" placeholder="title" type="text">
+        <input name="description" placeholder="description" type="text">
+        <button type="submit">Submit</button>
+    </form>
 </body>
 </html>
