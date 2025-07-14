3de1fd1fc36a22950eadf37bc2a32bb36e85074f
Darlan Rodrigues Schmeller <darlanschimeller@gmail.com>
2025-07-14 10:50:36 -0300

    Work on simple views with closures as controllers for routes

---
+++ resources/views/jobs/create.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create New Job Listing</title>
+</head>
+<body>
+    <h1>Create new job</h1>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,26 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/jobs', function () {
-    return view('welcome') . '<h1>Available Jobs</h1>';
+    $jobs = [
+       'Web Developer',
+       'Database Admin',
+       'Software Engineer',
+       'Systems Analyst'
+    ];
+
+    return view('jobs.index', compact('jobs'));
 })->name('jobs');
+
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
+})->name('jobs.create');
 
 Route::get('/posts/{id}', function (string $id) {
     return 'Post ' . $id;
@@ -16,4 +28,25 @@ Route::get('/posts/{id}', function (string $id) {
 
 Route::get('/posts/{id}/comments/{commentId}', function (string $id, string $commentId) {
     return 'Post ' . $id . $commentId;
+});
+
+Route::get('/test', function(Request $request) {
+    return [
+        'method' => $request->method(),
+        'url' => $request->url(),
+        'path' => $request->path(),
+        'fullUrl' => $request->fullUrl(),
+        'ip' => $request->ip(),
+        'userAgent' => $request->userAgent(),
+        'header' => $request->header()
+    ];
+});
+
+Route::get('/download', function() {
+    return response()->json(['name' => 'john doe'])->cookie('name', 'John doe');
+});
+
+Route::get('/read-cookie', function(Request $request) {
+    $cookieValue = $request->cookie('name');
+    return response()->json(['cookie' => $cookieValue]);
 });
