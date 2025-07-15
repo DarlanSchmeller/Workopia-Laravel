4bce8eb74a09bbecf809f509003a7fd2524a1d92
Darlan Rodrigues Schmeller <darlanschimeller@gmail.com>
2025-07-15 11:15:57 -0300

    Implement blade main layout, make other views extend main layout

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -1,11 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Create New Job Listing</title>
-</head>
-<body>
+@extends('layout')
+
+@section('title')
+    Create Job
+@endsection
+
+@section('content')
     <h1>Create new job</h1>
 
     <form action="/jobs" method="POST">
@@ -14,5 +13,4 @@
         <input name="description" placeholder="description" type="text">
         <button type="submit">Submit</button>
     </form>
-</body>
-</html>
+@endsection
