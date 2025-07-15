81fbc6b132b6d116670b48d842cebf37eeb0265e
Darlan Rodrigues Schmeller <darlanschimeller@gmail.com>
2025-07-15 11:44:57 -0300

    Implement more components and slots, delete old views

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -1,16 +1,9 @@
-@extends('layout')
-
-@section('title')
-    Create Job
-@endsection
-
-@section('content')
-    <h1>Create new job</h1>
-
+<x-layout>
+    <x-slot name="title">Create Job</x-slot>
     <form action="/jobs" method="POST">
         @csrf
         <input name="title" placeholder="title" type="text">
         <input name="description" placeholder="description" type="text">
         <button type="submit">Submit</button>
     </form>
-@endsection
+</x-layout>
