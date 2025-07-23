ff1d266913b40a224adadde89debe68a13f4b2c3
Darlan Rodrigues Schmeller <darlanschimeller@gmail.com>
2025-07-23 11:44:47 -0300

    Create header component, use url helper for header links

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,12 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title ?? 'Workopia | Find and List Jobs' }}</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/all.min.css"
+        integrity="sha512-DxV+EoADOkOygM4IR9yXP8Sb2qwgidEmeqAEmDKIOfPRQZOWbXCzLC6vjbZyy0vPisbH2SyW27+ddLVCN+OMzQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     @vite('resources/css/app.css')
 </head>
+
 <body class="bg-gray-100">
     <x-header />
     <h1>Layout Component</h1>
@@ -14,4 +19,5 @@
         {{ $slot }}
     </main>
 </body>
+
 </html>
