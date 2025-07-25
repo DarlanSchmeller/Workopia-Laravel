31bb103df641a5e991835cc5aad0cf9ca63eb12b
Darlan Rodrigues Schmeller <darlanschimeller@gmail.com>
2025-07-25 10:35:17 -0300

    Create TopBanner and BottomBanner components, add to layout

--- resources/views/components/button-link.blade.php
+++ resources/views/components/button-link.blade.php
@@ -9,7 +9,8 @@
 ])
 
 <a href="{{ $url }} {{ $attributes }}"
-    class="{{ $bgClass }} {{ $hoverClass }} {{ $textClass }} px-4 py-2 rounded hover:shadow-md transition duration-300 {{ $block ? 'block' : '' }}">
+    class="{{ $bgClass }} {{ $hoverClass }} {{ $textClass }} px-4 py-2 rounded hover:shadow-md
+    transition duration-300 {{ $block ? 'block' : '' }}">
     @if ($icon)
         <i class="fa fa-{{ $icon }} mr-1"></i>
     @endif
