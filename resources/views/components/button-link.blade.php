a54148a808ba0055b867995ef0cb88f219dadb70
Darlan Rodrigues Schmeller <darlanschimeller@gmail.com>
2025-07-25 09:58:09 -0300

    Implement button components for mobile menu, add dynamic props

--- resources/views/components/button-link.blade.php
+++ resources/views/components/button-link.blade.php
@@ -4,14 +4,14 @@
     'icon' => null,
     'bgClass' => 'bg-yellow-500',
     'hoverClass' => 'hover:bg-yellow-600',
-    'textColor' => 'text-black',
+    'textClass' => 'text-black',
+    'block' => false,
 ])
 
 <a href="{{ $url }} {{ $attributes }}"
-    class="{{ $bgClass }} {{ $hoverClass }} {{ $textColor }} px-4 py-2 rounded hover:shadow-md transition duration-300
-    {{ $active ? 'text-yellow-1000 font-bold' : '' }}">
+    class="{{ $bgClass }} {{ $hoverClass }} {{ $textClass }} px-4 py-2 rounded hover:shadow-md transition duration-300 {{ $block ? 'block' : '' }}">
     @if ($icon)
-        <i class="fa fa-{{ $icon }} "></i>
+        <i class="fa fa-{{ $icon }} mr-1"></i>
     @endif
     {{ $slot }}
 </a>
