1fbac6db9206a489131eb3225c0d385d9d7ad8af
Darlan Rodrigues Schmeller <darlanschimeller@gmail.com>
2025-08-19 11:44:26 -0300

    Add input validation for JobController store method, show errors on view

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -2,8 +2,18 @@
     <x-slot name="title">Create Job</x-slot>
     <form action="/jobs" method="POST">
         @csrf
-        <input name="title" placeholder="title" type="text">
-        <input name="description" placeholder="description" type="text">
+        <div class="my-5">
+            <input name="title" placeholder="title" type="text" value={{ old('title') }}>
+            @error('title')
+                <div class="text-red-500 mt-2 text-sm">{{ $message }}</div>
+            @enderror
+        </div>
+        <div class="mb-5">
+            <input name="description" placeholder="description" type="text" value={{ old('description') }}>
+            @error('description')
+                <div class="text-red-500 mt-2 text-sm">{{ $message }}</div>
+            @enderror
+        </div>
         <button type="submit">Submit</button>
     </form>
 </x-layout>
