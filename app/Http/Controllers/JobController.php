e05ff38621ee74b2276feb4730a2ee2bafdc8090
Darlan Rodrigues Schmeller <darlanschimeller@gmail.com>
2025-08-27 10:28:36 -0300

    Implement Policy authorization in JobController

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -4,13 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\View\View;
-use App\Models\Job;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use App\Models\Job;
 
 class JobController extends Controller
 {
+    use AuthorizesRequests;
+
     // @desc    Show all job listings
     // @route   GET /jobs
     public function index(): View
@@ -81,6 +84,9 @@ class JobController extends Controller
     // @route   GET /jobs/{$id}/edit
     public function edit(Job $job): View
     {
+        // Check if user is authorized
+        $this->authorize('update', $job);
+
         return view('jobs.edit')->with('job', $job);
     }
 
@@ -88,7 +94,10 @@ class JobController extends Controller
     // @route   PUT /jobs/{$id}
     public function update(Request $request, Job $job): string
     {
-       $validatedData = $request->validate([
+        // Check if user is authorized
+        $this->authorize('update', $job);
+
+        $validatedData = $request->validate([
             'title' => 'required|string|max:255',
             'description' => 'required|string|',
             'salary' => 'required|integer',
@@ -131,6 +140,9 @@ class JobController extends Controller
     // @route   DELETE /jobs/{$id}
     public function destroy(Job $job): RedirectResponse
     {
+        // Check if user is authorized
+        $this->authorize('delete', $job);
+
         // If logo, then delete it
         if($job->company_logo) {
             Storage::disk('public')->delete($job->company_logo);
